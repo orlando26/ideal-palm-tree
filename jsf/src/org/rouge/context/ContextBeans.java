17869f3929d71212424086553c0eab34d4fe903e
orlando26 <orlandolozaarr@gmail.com>
2016-06-17 19:54:08 -0500

    hibernate config

--- jsf/src/org/rouge/context/ContextBeans.java
+++ jsf/src/org/rouge/context/ContextBeans.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import org.rouge.bean.Auth;
+
 @ManagedBean
 @SessionScoped
 public class ContextBeans implements Serializable{
@@ -13,6 +15,35 @@ public class ContextBeans implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private Auth authentication;
+
+	/**
+	 * @return the auth
+	 */
+	
+
+	public void print(){
+		System.out.println("dsadj;aslkdl;lkda");
+	}
+
+	/**
+	 * @return the authentication
+	 */
+	public Auth getAuthentication() {
+		if (authentication == null){
+			authentication = new Auth();
+		}
+		return authentication;
+	}
+
+	/**
+	 * @param authentication the authentication to set
+	 */
+	public void setAuthentication(Auth authentication) {
+		this.authentication = authentication;
+	}
+
 	
 	
 }
