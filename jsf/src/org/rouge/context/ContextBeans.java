2410ff984d17168dd8e92ff98060200e6eea54e7
orlando26 <orlandolozaarr@gmail.com>
2016-06-20 00:01:33 -0500

    admin page

--- jsf/src/org/rouge/context/ContextBeans.java
+++ jsf/src/org/rouge/context/ContextBeans.java
@@ -6,6 +6,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 import org.rouge.bean.Auth;
+import org.rouge.bean.Register;
 
 @ManagedBean
 @SessionScoped
@@ -17,6 +18,8 @@ public class ContextBeans implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Auth authentication;
+	
+	private Register register;
 
 	/**
 	 * @return the authentication
@@ -33,6 +36,23 @@ public class ContextBeans implements Serializable{
 	 */
 	public void setAuthentication(Auth authentication) {
 		this.authentication = authentication;
+	}
+
+	/**
+	 * @return the register
+	 */
+	public Register getRegister() {
+		if (register == null){
+			register = new Register();
+		}
+		return register;
+	}
+
+	/**
+	 * @param register the register to set
+	 */
+	public void setRegister(Register register) {
+		this.register = register;
 	}
 
 	
