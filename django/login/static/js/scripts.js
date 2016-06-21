128c786dc5910b67e4454cb56ea6856d406c837d
orlando26 <orlandolozaarr@gmail.com>
2016-06-21 15:51:56 -0500

    django folder deleted

--- django/login/static/js/scripts.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $('.boton_numero').click(function(){
-        var text = $('#username-textfield').val();
-        $('#username-textfield').val(text + $(this).text());
-    });
-});
